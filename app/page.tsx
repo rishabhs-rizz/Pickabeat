188355879ae56c3bf51ec5a4d92bdef567449bcd
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-08-04 14:59:01 +0530

    added next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Appbar } from "./components/Appbar";
+
 export default function Home() {
-  return <div className="bg-red-500">hello</div>;
+  return (
+    <main>
+      <Appbar />
+    </main>
+  );
 }
