98fd7f98250cfe0bcb0091709c6fe1be03d9c3df
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-08-05 18:38:22 +0530

    explicitly added id key to session's type for obtaining user's id(user.id)

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,15 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
