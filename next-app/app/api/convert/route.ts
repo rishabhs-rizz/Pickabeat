f0b5f3a4d105583ccd402fc7fc586a6c06717039
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-09-06 17:45:22 +0530

    logic to convert uri's

---
+++ next-app/app/api/convert/route.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+function addCorsHeaders(res: NextResponse) {
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  return res;
+}
+
+export async function OPTIONS() {
+  const res = NextResponse.json({});
+  return addCorsHeaders(res);
+}
+
+export async function GET(req: NextRequest) {
+  const trackId = req.nextUrl.searchParams.get("trackId");
+
+  try {
+    const response = await axios.get(
+      `https://backend.canum.xyz/api2/api/song/convert?spotifyURL=https://open.spotify.com/track/${trackId}`
+    );
+    const res = NextResponse.json({ data: response.data }, { status: 200 });
+    return addCorsHeaders(res);
+  } catch (error: any) {}
+}
--- next-app/lib/Handlers.ts
+++ next-app/lib/Handlers.ts
@@ -1,4 +1,23 @@
-export const handleJoinStrem = () => {};
+import axios from "axios";
+export const handleMusicPlaying = async (uri: string) => {
+  const trackId = uri.split(":").pop();
+
+  if (!trackId) {
+    console.error("Invalid Spotify URI:", uri);
+    return;
+  }
+
+  console.log("Playing track with ID:", trackId);
+
+  try {
+    const res = await axios.get(
+      `http://localhost:3000/api/convert?trackId=${trackId}`
+    );
+    console.log("Conversion response in handlers.ts:", res.data);
+  } catch (error) {
+    console.error("Error playing track in handlers.ts:", error);
+  }
+};
 
 export const handleGetStarted = () => {
   const AUTH_URL = process.env.AUTH_URL;
