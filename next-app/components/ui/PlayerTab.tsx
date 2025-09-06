9f3195bd2184711452d09bc6af90fc6ebb778994
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-09-06 17:40:47 +0530

    MusicPlayer Component

---
+++ next-app/components/ui/PlayerTab.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import SpotifyPlayer from "react-spotify-web-playback";
+
+export default function Player({
+  accessToken,
+  trackUri,
+}: {
+  accessToken: string;
+  trackUri: string | undefined;
+}) {
+  const [play, setPlay] = useState(false);
+
+  useEffect(() => setPlay(true), [trackUri]);
+
+  if (!accessToken) return null;
+  return (
+    <SpotifyPlayer
+      token={accessToken}
+      showSaveIcon
+      callback={(state) => {
+        if (!state.isPlaying) setPlay(false);
+      }}
+      play={play}
+      uris={trackUri ? [trackUri] : []}
+    />
+  );
+}
