dd543ee65a602e29f3ef2f602fe64c56d8f44d84
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-09-17 15:08:00 +0530

    react-youtube lib and LRC Parser fn

--- next-app/lib/utils.ts
+++ next-app/lib/utils.ts
@@ -1,5 +1,20 @@
+import { LyricLine } from "@/app/PBplayer/page";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function parseLRC(lrc: string): LyricLine[] {
+  return lrc
+    .split("\n")
+    .map((line) => {
+      const m = line.match(/\[(\d{2}):(\d{2}(?:\.\d{2})?)\](.*)/);
+      if (!m) return null;
+      const min = parseInt(m[1], 10);
+      const sec = parseFloat(m[2]);
+      const text = m[3].trim();
+      return { time: min * 60 + sec, text };
+    })
+    .filter(Boolean) as LyricLine[];
 }
