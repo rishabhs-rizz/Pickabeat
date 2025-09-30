5e1b346ad854d39609c4a950eb9c1e41194f00d0
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-09-30 14:43:20 +0530

    ws-connection through route 200

--- next-app/app/stream/[roomId]/page.tsx
+++ next-app/app/stream/[roomId]/page.tsx
@@ -1,7 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { StreamRoom } from "@/components/StreamRoom";
+
 export default function StreamPage() {
-  return (
-    <div className="h-screen flex justify-center items-center bg-black text-white text-2xl">
-      aa liye re stream page par
-    </div>
-  );
+  const params = useParams();
+  const roomId = params.roomId as string;
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  async function TicketCall() {
+    const token = await axios
+      .get("http://localhost:3000/api/ws-ticket")
+      .then((res) => res.data.token);
+    console.log("token :", token);
+    return token;
+  }
+
+  useEffect(() => {
+    async function connectWS() {
+      const token = await TicketCall();
+      const ws = new WebSocket(`ws://localhost:8080?token=${token}`);
+      if (ws) {
+        ws.onopen = () => {
+          console.log("WebSocket connection established");
+          setSocket(ws);
+          ws.send(
+            JSON.stringify({
+              type: "join",
+              roomId,
+            })
+          );
+        };
+      }
+    }
+
+    connectWS();
+  }, [roomId]);
+
+  if (!socket) {
+    return <div>Loading...</div>;
+  }
+  return <StreamRoom roomId={roomId} socket={socket} />;
 }
